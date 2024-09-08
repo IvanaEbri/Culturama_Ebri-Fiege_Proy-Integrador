5e57e8bd43b4137db0ef56cd686295c4283b4b6b
= <3ivana.ebri@gmail.com>
2024-09-08 18:21:03 -0300

    formulario registro

--- culturama/user/forms.py
+++ culturama/user/forms.py
@@ -1,15 +1,33 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
+from django import forms
 
 
-class CustomUserCreationForm(UserCreationForm):
+class UserCreationForm(UserCreationForm):
+    username = forms.CharField(label="Username", max_length=20)
+    first_name = forms.CharField(label="Nombre", max_length=20)
+    last_name = forms.CharField(label="Apellido", max_length=20)
+    email = forms.EmailField(label="Email")
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirme su contraseña", widget=forms.PasswordInput)
+    is_staff = forms.BooleanField(label="Administrador", required=False)
 
     class Meta(UserCreationForm):
         model = User
-        fields = UserCreationForm.Meta.fields
+        fields = ['username', 'first_name', 'last_name', 'email','is_staff' ,'password1', 'password2']
+        labels = {
+            'username': 'Username',
+            'password1': 'Contraseña',
+            'password2': 'Confirme su contraseña',
+            'is_staff': 'Administrador',
+        }
+        widgets = {
+            'password1': forms.PasswordInput(),
+            'password2': forms.PasswordInput(),
+        }
 
 
-class CustomUserChangeForm(UserChangeForm):
+class UserChangeForm(UserChangeForm):
 
     class Meta:
         model = User
--- culturama/user/views.py
+++ culturama/user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.views.generic import View
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 # Create your views here.
+
+
+class RegisterView (CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'register.html'
+
+    def form_valid(self, form):
+        # Procesar el formulario si es válido
+        user = form.save(commit=False)
+        # Podemos hacer algo
+        user.save()
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        # Agregar mensajes de error a la lista de mensajes
+        for field, errors in form.errors.items():
+            for error in errors:
+                messages.error(self.request, f"{error}")
+        return super().form_invalid(form)
