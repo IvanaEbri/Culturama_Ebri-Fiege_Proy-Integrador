8a00e7661ad0a43d79cb6f285f1fbde7f373a083
= <3ivana.ebri@gmail.com>
2024-09-08 18:30:24 -0300

    inicio de sesion

--- culturama/user/urls.py
+++ culturama/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import UserRegisterView, UserLoginView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='Registro'),
+    path('register/', UserRegisterView.as_view(), name='Registro'),
+    path('login/', UserLoginView.as_view(), name='Login'),
 ]
--- culturama/user/views.py
+++ culturama/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
 from django.views.generic import View
@@ -9,7 +10,7 @@ from django.contrib import messages
 # Create your views here.
 
 
-class RegisterView (CreateView):
+class UserRegisterView (CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'register.html'
@@ -26,4 +27,26 @@ class RegisterView (CreateView):
         for field, errors in form.errors.items():
             for error in errors:
                 messages.error(self.request, f"{error}")
+        return super().form_invalid(form)
+
+class UserLoginView (LoginView):
+    template_name = 'login.html'
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user)
+        
+        # Redirigir según el tipo de usuario
+        if user.is_active:
+            redirect('home')
+        #     if user.cliente:
+        #         return redirect('home')
+        #     else:
+        #         return redirect('home_admin')
+        # else:
+        #     return redirect('home')
+
+    def form_invalid(self, form):
+        messages.error(
+            self.request, "Los datos ingresados son incorrectos. Por favor, inténtalo de nuevo."
+        )  # Mensaje de error
         return super().form_invalid(form)
